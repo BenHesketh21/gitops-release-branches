0f4324aa40325b615bdd18f9319bca700e78450f
BenHesketh21 <heskethben0@gmail.com>
2024-05-05 21:31:06 +0100

    Making changes
    Signed-off-by: BenHesketh21 <heskethben0@gmail.com>

--- app1/app.py
+++ app1/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "App1 Hello!!!!!!!!!!"
+    return "App1 Hello!!!!!!!!!!!"
 
 if __name__ == "__main__":
     app.run('0.0.0.0', port=5000)
